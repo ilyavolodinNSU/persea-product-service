89bc4dea32721576e353cf3744a211175aef21da
Ilya <i.volodin@g.nsu.ru>
2026-05-23 15:02:20 +0700

    added brands and categories sync

--- debezium/init-connectors.sh
+++ debezium/init-connectors.sh
@@ -7,3 +7,7 @@ curl -X POST http://kafka-connect:8083/connectors \
 curl -X POST http://kafka-connect:8083/connectors \
   -H "Content-Type: application/json" \
   -d @/product-outbox-connector-config.json
+
+curl -X POST http://kafka-connect:8083/connectors \
+  -H "Content-Type: application/json" \
+  -d @/user-action-connector-config.json
